143b53dfde080c6dcbbd82e850055a943cac6d9f
Yohenba Kshetrimayum <66858085+Yohenba18@users.noreply.github.com>
2023-06-19 15:48:07 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=yohenba/user-service
 sudo docker pull $DOCKER_IMAGE
 sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-sudo docker run -d -p 8080:8080 $DOCKER_IMAGE
+sudo docker run -d -p 8081:8081 $DOCKER_IMAGE
